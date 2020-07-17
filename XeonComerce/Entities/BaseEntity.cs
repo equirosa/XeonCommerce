9d936e645e02fdc20317b078da6b94c5f4de1d22
jdiego98 <jarguelloq@ucenfotec.ac.cr>
2020-07-17 17:33:47 -0600

    Agregue interfaces y clases abstractas de la arquitectura

---
+++ XeonComerce/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class BaseEntity
+    {
+        public String GetEntityInformation()
+        {
+            var dump = ObjectDumper.Dump(this);
+            return dump;
+        }
+    }
+}
