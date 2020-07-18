431557a08882a4bcd746e9467da62db5fa780e28
Eduardo Quiros <equirosa@ucenfotec.ac.cr>
2020-07-18 02:23:17 -0700

    Crear CategoriaMapper

---
+++ XeonComerce/DataAccess/Mapper/CategoriaMapper.cs
@@ -0,0 +1,55 @@
+﻿using DataAccess.Dao;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Mapper
+{
+    public class CategoriaMapper : EntityMapper, IObjectMapper, ISqlStaments
+    {
+        private const string DB_COL_ID = "ID";
+        private const string DB_COL_VALOR = "VALOR";
+
+        public BaseEntity BuildObject(Dictionary<string, object> row)
+        {
+            var categoria = new Categoria()
+            {
+                Id = getIntValue(row, DB_COL_ID),
+                Valor = getStringValue(row, DB_COL_VALOR)
+            };
+
+            return categoria;
+        }
+
+        public List<BaseEntity> BuildObjects(List<Dictionary<string, object>> lstRows)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetCreateStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetDeleteStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetriveAllStatement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetRetriveStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlOperation GetUpdateStatement(BaseEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
