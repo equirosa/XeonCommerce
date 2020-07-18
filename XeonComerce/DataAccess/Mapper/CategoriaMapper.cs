d69bd42f515559db0c7b281fe7f9763b9736393b
Eduardo Quiros <equirosa@ucenfotec.ac.cr>
2020-07-18 02:33:56 -0700

    finalizar CategoriaMapper

--- XeonComerce/DataAccess/Mapper/CategoriaMapper.cs
+++ XeonComerce/DataAccess/Mapper/CategoriaMapper.cs
@@ -36,27 +36,43 @@ namespace DataAccess.Mapper
 
         public SqlOperation GetCreateStatement(BaseEntity entity)
         {
-            throw new NotImplementedException();
+            var operation = new SqlOperation { ProcedureName = "CRE_CATEGORIA_PR" };
+
+            var c = (Categoria)entity;
+            operation.AddVarcharParam(DB_COL_VALOR, c.Valor);
+            // TODO: determinar si es necesario tambien agregar el Id de la categoria a crear
+            return operation;
         }
 
         public SqlOperation GetDeleteStatement(BaseEntity entity)
         {
-            throw new NotImplementedException();
+            var operation = new SqlOperation { ProcedureName = "DEL_CATEGORIA_PR" };
+            var c = (Categoria)entity;
+            operation.AddIntParam(DB_COL_ID, c.Id);
+            return operation;
         }
 
         public SqlOperation GetRetriveAllStatement()
         {
-            throw new NotImplementedException();
+            var operation = new SqlOperation { ProcedureName = "RET_ALL_CATEGORIA_PR" };
+            return operation;
         }
 
         public SqlOperation GetRetriveStatement(BaseEntity entity)
         {
-            throw new NotImplementedException();
+            var operation = new SqlOperation { ProcedureName = "RET_CATEGORIA_PR" };
+            var c = (Categoria)entity;
+            operation.AddIntParam(DB_COL_ID, c.Id);
+            return operation;
         }
 
         public SqlOperation GetUpdateStatement(BaseEntity entity)
         {
-            throw new NotImplementedException();
+            var operation = new SqlOperation { ProcedureName = "RET_CATEGORIA_PR" };
+            var c = (Categoria)entity;
+            operation.AddIntParam(DB_COL_ID, c.Id);
+            operation.AddVarcharParam(DB_COL_VALOR, c.Valor);
+            return operation;
         }
     }
 }
