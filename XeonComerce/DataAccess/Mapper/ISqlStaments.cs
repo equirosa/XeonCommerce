af0355fbb98de53c4c4401f31c0b68a52e5937e5
Kamil Sauma <kamilsauma07@gmail.com>
2020-07-17 18:52:17 -0600

    Se agregó el CrudFactory, Mapper, Management, Controller y Stored Procedures de Comercio y Contraseña.

---
+++ XeonComerce/DataAccess/Mapper/ISqlStaments.cs
@@ -0,0 +1,16 @@
+﻿
+
+using DataAccessLayer.Dao;
+using Entities;
+
+namespace DataAccessLayer.Mapper
+{
+    public interface ISqlStaments
+    {
+        SqlOperation GetCreateStatement(BaseEntity entity);
+        SqlOperation GetRetriveStatement(BaseEntity entity);
+        SqlOperation GetRetriveAllStatement();
+        SqlOperation GetUpdateStatement(BaseEntity entity);
+        SqlOperation GetDeleteStatement(BaseEntity entity);
+    }
+}
