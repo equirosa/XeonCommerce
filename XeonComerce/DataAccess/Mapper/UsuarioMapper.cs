186eb83cb3e41d16d242295e41e87c77038f6d44
Eduardo Quiros <equirosa@ucenfotec.ac.cr>
2020-07-20 08:52:54 -0700

    Crear UsuarioMapper

---
+++ XeonComerce/DataAccess/Mapper/UsuarioMapper.cs
@@ -0,0 +1,116 @@
+﻿using DataAccess.Dao;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Mapper
+{
+    public class UsuarioMapper : EntityMapper, ISqlStaments, IObjectMapper
+    {
+        private const string DB_COL_ID = "ID";
+        private const string DB_COL_NOMBRE = "NOMBRE";
+        private const string DB_COL_APELLIDO1 = "APELLIDO1";
+        private const string DB_COL_APELLIDO2 = "APELLIDO2";
+        private const string DB_COL_GENERO = "GENERO";
+        private const string DB_COL_FECHA_NACIMIENTO = "FECHA_NACIMIENTO";
+        private const string DB_COL_CORREO_ELECTRONICO = "CORREO_ELECTRONICO";
+        private const string DB_COL_TELEFONO = "TELEFONO";
+        private const string DB_COL_ID_DIRECCION = "ID_DIRECCION";
+        private const string DB_COL_ESTADO = "ESTADO";
+
+        public SqlOperation GetCreateStatement(BaseEntity entity)
+        {
+            var operation = new SqlOperation { ProcedureName = "CRE_COMERCIO_PR" };
+
+            var c = (Usuario)entity;
+            operation.AddVarcharParam(DB_COL_ID, c.Id);
+            operation.AddVarcharParam(DB_COL_NOMBRE, c.Nombre);
+            operation.AddVarcharParam(DB_COL_APELLIDO1, c.ApellidoUno);
+            operation.AddVarcharParam(DB_COL_APELLIDO2, c.ApellidoDos);
+            operation.AddVarcharParam(DB_COL_GENERO, c.Genero);
+            operation.AddDateTimeParam(DB_COL_FECHA_NACIMIENTO, c.FechaNacimiento);
+            operation.AddVarcharParam(DB_COL_CORREO_ELECTRONICO, c.CorreoElectronico);
+            operation.AddVarcharParam(DB_COL_TELEFONO, c.NumeroTelefono);
+            operation.AddIntParam(DB_COL_ID_DIRECCION, c.IdDireccion);
+            return operation;
+        }
+
+
+        public SqlOperation GetRetriveStatement(BaseEntity entity)
+        {
+            var operation = new SqlOperation { ProcedureName = "RET_COMERCIO_PR" };
+
+            var c = (Usuario)entity;
+            operation.AddVarcharParam(DB_COL_ID, c.Id);
+
+            return operation;
+        }
+
+        public SqlOperation GetRetriveAllStatement()
+        {
+            var operation = new SqlOperation { ProcedureName = "RET_ALL_COMERCIO_PR" };
+            return operation;
+        }
+
+        public SqlOperation GetUpdateStatement(BaseEntity entity)
+        {
+            var operation = new SqlOperation { ProcedureName = "UPD_COMERCIO_PR" };
+
+            var c = (Usuario)entity;
+            operation.AddVarcharParam(DB_COL_ID, c.Id);
+            operation.AddVarcharParam(DB_COL_NOMBRE, c.Nombre);
+            operation.AddVarcharParam(DB_COL_APELLIDO1, c.ApellidoUno);
+            operation.AddVarcharParam(DB_COL_APELLIDO2, c.ApellidoDos);
+            operation.AddVarcharParam(DB_COL_GENERO, c.Genero);
+            operation.AddDateTimeParam(DB_COL_FECHA_NACIMIENTO, c.FechaNacimiento);
+            operation.AddVarcharParam(DB_COL_CORREO_ELECTRONICO, c.CorreoElectronico);
+            operation.AddVarcharParam(DB_COL_TELEFONO, c.NumeroTelefono);
+            operation.AddIntParam(DB_COL_ID_DIRECCION, c.IdDireccion);
+
+            return operation;
+        }
+
+        public SqlOperation GetDeleteStatement(BaseEntity entity)
+        {
+            var operation = new SqlOperation { ProcedureName = "DEL_COMERCIO_PR" };
+
+            var c = (Usuario)entity;
+            operation.AddVarcharParam(DB_COL_ID, c.Id);
+            return operation;
+        }
+
+        public List<BaseEntity> BuildObjects(List<Dictionary<string, object>> lstRows)
+        {
+            var lstResults = new List<BaseEntity>();
+
+            foreach (var row in lstRows)
+            {
+                var comercio = BuildObject(row);
+                lstResults.Add(comercio);
+            }
+
+            return lstResults;
+        }
+
+        public BaseEntity BuildObject(Dictionary<string, object> row)
+        {
+            var comercio = new Usuario
+            {
+                Id = GetStringValue(row, DB_COL_ID),
+                Nombre = GetStringValue(row, DB_COL_NOMBRE),
+                ApellidoUno = GetStringValue(row, DB_COL_APELLIDO1),
+                ApellidoDos = GetStringValue(row, DB_COL_APELLIDO2),
+                Genero = GetStringValue(row,DB_COL_GENERO),
+                FechaNacimiento = GetDateValue(row, DB_COL_FECHA_NACIMIENTO),
+                CorreoElectronico = GetStringValue(row, DB_COL_CORREO_ELECTRONICO),
+                NumeroTelefono = GetStringValue(row, DB_COL_TELEFONO),
+                IdDireccion = GetIntValue(row, DB_COL_ID_DIRECCION),
+                Estado = GetStringValue(row, DB_COL_ESTADO)
+            };
+
+            return comercio;
+        }
+
+    }
+}
