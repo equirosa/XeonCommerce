3259a3be4eec7fc46636c81eda7dd9522d74b0c4
salpizarr <salpizarr@ucenfotec.ac.cr>
2020-07-25 14:48:44 -0600

    Crear promocion

---
+++ XeonComerce/WebUI/ClientApp/src/app/_models/productoServicio.ts
@@ -0,0 +1,10 @@
+export class ProductoServicio {
+  id: number;
+  tipo: number;
+  nombre: string;
+  precio: number;
+  cantidad: number;
+  descuento: number;
+  idComercio: string;
+  duracion: number;
+}
