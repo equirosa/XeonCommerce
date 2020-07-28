19b580852ca092163dadee8dd855b7afa854e8c3
Eduardo Quiros <equirosa@ucenfotec.ac.cr>
2020-07-28 01:18:35 -0700

    crear clase config

---
+++ XeonComerce/Entities/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Entities
+{
+    public class Config: BaseEntity
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+        [JsonPropertyName("valor")]
+        public decimal Valor { get; set; }
+
+        public Config() { }
+
+    }
+}
