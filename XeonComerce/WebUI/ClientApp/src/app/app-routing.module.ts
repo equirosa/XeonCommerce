3ba220e2f00bc93d507ca0587ea35b6262ba6256
Kamil Sauma <kamilsauma07@gmail.com>
2020-08-04 12:19:27 -0600

    Carrito inicio

--- XeonComerce/WebUI/ClientApp/src/app/app-routing.module.ts
+++ XeonComerce/WebUI/ClientApp/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { CarritoComponent } from './carrito/carrito.component';
 import { PerfilComercioComponent } from './perfil/comercio/comercio.component';
 import { DiaFeriadoComponent } from './diaferiado/dia-feriado.component';
 import { PromocionComponent } from './promocion/promocion.component';
@@ -47,7 +48,9 @@ const routes: Routes = [
     { path: 'diaferiado', component: DiaFeriadoComponent, canActivate: [AuthGuard]},
     { path: 'comercio/:id', component: PerfilComercioComponent, canActivate: [AuthGuard]},
   // { path: 'empleado', component: EmpleadoAdminComponent },
-    { path: 'rol', component: ComercioRolComponent, canActivate: [AuthGuard] },
+  { path: 'config', component: ConfigComponent, canActivate: [AuthGuard] },
+  { path: 'rol', component: ComercioRolComponent, canActivate: [AuthGuard] },
+  { path: 'carrito', component: CarritoComponent, canActivate: [AuthGuard] },
     { path: 'sucursales', component: SucursalesComponent, canActivate: [AuthGuard] },
     { path: 'config', component: ConfigComponent, canActivate: [AuthGuard] },
     { path: 'listar-usuarios', component: ListarUsuariosComponent, canActivate: [AuthGuard]},
---
+++ XeonComerce/WebUI/ClientApp/src/app/carrito/carrito.component.ts
@@ -0,0 +1,95 @@
+import { Impuesto } from './../_models/impuesto';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ImpuestoService } from './../_services/impuesto.service';
+import { ProductoService } from './../_services/producto.service';
+import { AccountService } from './../_services/account.service';
+import { MensajeService } from './../_services/mensaje.service';
+import { CarritoService } from './../_services/carrito.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito',
+  templateUrl: './carrito.component.html',
+  styleUrls: ['./carrito.component.css']
+})
+export class CarritoComponent implements OnInit {
+
+	productos: any[];
+	displayedColumns: string[] = ['id', 'nombre', 'precio', 'cantidad', 'impuesto', 'editar', 'eliminar'];
+	datos;
+	user: any;
+	impuestos: Impuesto[];
+  constructor(private carritoService:CarritoService, private mensajeService:MensajeService, private accountService:AccountService, private productoService:ProductoService, private impuestoService:ImpuestoService) {
+	this.accountService.user.subscribe(x => {
+		this.user = x;
+	});
+ 	}
+
+	 @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  ngOnInit(): void {
+	  this.getImpuestos();
+	  this.getProductos();
+  }
+
+  getImpuestos(): void {
+	  this.impuestoService.getImpuesto().subscribe((impuestos)=>this.impuestos=impuestos);
+  }
+
+  getProductos(): void {
+
+	this.carritoService.get(this.user.id).subscribe((items)=>{
+
+		this.productoService.getProducto()
+		.subscribe(productos => {
+			productos = productos.filter(a=>items.find((i)=>i.idProducto==a.id));
+			this.productos = productos.map((i)=>{
+				let a = items.find((e)=>e.idProducto==i.id);
+				let imp = this.impuestos.find((e)=>e.id==i.impuesto);
+				if(a) i["cantidadCarrito"] = a.cantidad;
+				if(imp) (i["porcientoImpuesto"] = imp.valor, i["nombreImpuesto"]=imp.nombre);
+				return i;
+			});
+			console.log(this.productos);
+			this.datos = new MatTableDataSource(this.productos);
+			this.datos.sort = this.sort;
+			})
+
+	});
+}
+
+
+limpiar(): void {
+
+}
+
+getCosto(tabla, conImpuestos){
+	if(tabla && tabla.filteredData){
+		return tabla.filteredData.reduce((acc, cv)=>{
+	if(conImpuestos) 
+		return acc+= ((cv.precio-cv.descuento)*cv.cantidadCarrito)*((cv.porcientoImpuesto/100)+1);
+		else
+		return acc+= ((cv.precio-cv.descuento)*cv.cantidadCarrito);
+	}, 0);
+	}
+}
+
+getCantidad(tabla){
+	if(tabla && tabla.filteredData){
+		return tabla.filteredData.reduce((acc, cv)=>{
+		return acc+= cv.cantidadCarrito;
+	}, 0);
+	}
+}
+
+calcularDescuento(element){
+	let a = Number(100-(element.precio-element.descuento)/(element.precio)*100);
+	return Math.round(a) == a ? a:a.toFixed(2);
+}
+
+calcularPrecio(element){
+	return element.descuento ? element.precio-element.descuento:element.precio;
+}
+
+}
