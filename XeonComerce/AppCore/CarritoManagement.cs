49c4540070f7a37bbe2dc0aa1e0fdc2863ae81ef
Kamil Sauma <kamilsauma07@gmail.com>
2020-08-04 02:24:14 -0600

    Logica de carrito terminada falta service y parte gráfica.

---
+++ XeonComerce/AppCore/CarritoManagement.cs
@@ -0,0 +1,48 @@
+﻿using DataAccess.Crud;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore
+{
+    public class CarritoManagement
+    {
+        private CarritoCrudFactory crud;
+
+        public CarritoManagement()
+        {
+            crud = new CarritoCrudFactory();
+        }
+
+        public void Create(Carrito obj)
+        {
+            crud.Create(obj);
+        }
+
+        public List<Carrito> RetriveAll(Carrito obj)
+        {
+            return crud.RetrieveAll<Carrito>(obj);
+        }
+
+        public Carrito RetriveById(Carrito obj)
+        {
+            return crud.Retrieve<Carrito>(obj);
+        }
+
+        public void Update(Carrito obj)
+        {
+            crud.Update(obj);
+        }
+
+        public void Delete(Carrito obj)
+        {
+            crud.Delete(obj);
+        }
+
+        public void DeleteAll(Carrito obj)
+        {
+            crud.DeleteAll(obj);
+        }
+    }
+}
---
+++ XeonComerce/DataAccess/Crud/CarritoCrudFactory.cs
@@ -0,0 +1,81 @@
+﻿using DataAccess.Dao;
+using DataAccess.Mapper;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Crud
+{
+    public class CarritoCrudFactory : CrudFactory
+    {
+        CarritoMapper mapper;
+
+        public CarritoCrudFactory() : base()
+        {
+            mapper = new CarritoMapper();
+            dao = SqlDao.GetInstance();
+        }
+
+        public override void Create(BaseEntity entity)
+        {
+            var e = (Carrito)entity;
+            var sqlOperation = mapper.GetCreateStatement(e);
+            dao.ExecuteProcedure(sqlOperation);
+        }
+
+        public override T Retrieve<T>(BaseEntity entity)
+        {
+            var lstResult = dao.ExecuteQueryProcedure(mapper.GetRetriveStatement(entity));
+            var dic = new Dictionary<string, object>();
+            if (lstResult.Count > 0)
+            {
+                dic = lstResult[0];
+                var objs = mapper.BuildObject(dic);
+                return (T)Convert.ChangeType(objs, typeof(T));
+            }
+
+            return default(T);
+        }
+
+        public List<T> RetrieveAll<T>(BaseEntity entity)
+        {
+            var lst = new List<T>();
+
+            var lstResult = dao.ExecuteQueryProcedure(mapper.GetRetriveAllStatement(entity));
+            var dic = new Dictionary<string, object>();
+            if (lstResult.Count > 0)
+            {
+                var objs = mapper.BuildObjects(lstResult);
+                foreach (var arc in objs)
+                {
+                    lst.Add((T)Convert.ChangeType(arc, typeof(T)));
+                }
+            }
+
+            return lst;
+        }
+
+        public override void Update(BaseEntity entity)
+        {
+            var e = (Carrito)entity;
+            dao.ExecuteProcedure(mapper.GetUpdateStatement(e));
+        }
+
+        public override void Delete(BaseEntity entity)
+        {
+            var e = (Carrito)entity;
+            dao.ExecuteProcedure(mapper.GetDeleteStatement(e));
+        }
+        public void DeleteAll(BaseEntity entity)
+        {
+            var e = (Carrito)entity;
+            dao.ExecuteProcedure(mapper.GetDeleteAllStatement(e));
+        }
+
+        public override List<T> RetrieveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
