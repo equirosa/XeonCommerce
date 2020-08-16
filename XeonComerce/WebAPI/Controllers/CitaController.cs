f472d954da0d43837ab5510baa07ec54c99024ee
jdiego98 <jarguelloq@ucenfotec.ac.cr>
2020-08-16 01:21:19 -0600

    Enviar Notificacion al crear la cita

--- XeonComerce/WebAPI/Controllers/CitaController.cs
+++ XeonComerce/WebAPI/Controllers/CitaController.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using AppCore;
 using Entities;
+using Management;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace WebAPI.Controllers
 {
@@ -20,7 +23,15 @@ namespace WebAPI.Controllers
             try
             {
                 var citaManag = new CitaManagement();
+                var um = new UsuarioManagement();
+
                 citaManag.Create(citaProducto);
+
+                Usuario usuario = um.RetrieveById(new Usuario { Id = citaProducto.IdCliente });
+
+                var info = citaProducto.HoraInicio.ToString("MM/dd/yy");
+
+                Excecute(info, usuario).Wait();
 
                 return Ok();
             } 
@@ -51,6 +62,21 @@ namespace WebAPI.Controllers
             {
                 return StatusCode(500, new { msg = ex.Message });
             }
+        }
+
+
+        private static async Task Excecute(string info, Usuario user)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("brutchm@ucenfotec.ac.cr", "GetItSafely");
+            var subject = "Inofrmacion de la cita con GetItSafely";
+            //var to = new EmailAddress(user.CorreoElectronico, user.Nombre);
+            var to = new EmailAddress("jarguelloq@ucenfotec.ac.cr", user.Nombre);
+            var plainTextContent = ("Bienvenido a GetItSafely se ha agendado su cita, información: " + info + ".");
+            var htmlContent = "<strong>Gracias por preferir GetItSafely, la información de su cita es: '" + info + "' ." + "</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
 
     }
